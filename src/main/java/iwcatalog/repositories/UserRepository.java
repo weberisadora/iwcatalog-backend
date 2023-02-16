2237533bb6e22a323ba102f1f7f987bec6a632c8
Isadora Weber <weberisadora@gmail.com>
2023-02-16 13:35:05 -0300

    adds properties to connect with postresql database

--- src/main/java/iwcatalog/repositories/UserRepository.java
+++ src/main/java/iwcatalog/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import iwcatalog.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    List<User> findByFirstName(String firstName);
 }
